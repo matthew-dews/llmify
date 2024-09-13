c7a1fb036f03fb5cd5462575705e22c76204c9bf
Corinne Corinfinite <corinfinite@gmail.com>
2024-09-12 23:36:02 -0400

    switch to poetry2nix

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # llmify
 
-# Usage
-./llmify.py args
+To get started, run the following:
+
+```
+$ nix develop
+$ poetry run python -m sample_package
+Hello, world!
+```
 
 # Formating & type checking
 Run `lint.sh`, which will setup a venv and then run the formatter and type checker.
-
-# Packaging
-Run `bundle.sh`, which will setup a venv and then run PyInstaller.
--- flake.nix
+++ flake.nix
@@ -1,50 +1,47 @@
 {
-  description = "A Nix flake for the llmify.py script";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  };
-
-  outputs = { self, nixpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      poetry2nix,
+    }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      pythonPackages = pkgs.python3Packages;
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
-      packages.${system}.llmify = pythonPackages.buildPythonApplication rec {
-        pname = "llmify";
-        version = "1.0.0";
+      packages = forAllSystems (
+        system:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
+        in
+        {
+          default = mkPoetryApplication { projectDir = self; };
+        }
+      );
 
-        src = ./.;
-
-        format = "other";  # Skip the default build process that expects setup.py
-
-        propagatedBuildInputs = with pythonPackages; [
-          # Add required Python packages here
-          # Example: requests
-          # requests
-        ];
-
-        # Override build phases
-        buildPhase = "echo 'No build required'";
-        installPhase = ''
-          mkdir -p $out/bin
-          cp llmify.py $out/bin/llmify
-          chmod +x $out/bin/llmify
-        '';
-
-        # Ensure the script runs with the correct interpreter and PYTHONPATH
-        postFixup = ''
-          wrapProgram $out/bin/llmify --prefix PYTHONPATH : ${pythonPackages.concatPackages propagatedBuildInputs}/lib/python${pythonPackages.pythonVersion}/site-packages
-        '';
-
-        meta = with pkgs.lib; {
-          description = "llmify Python script";
-          homepage = "https://github.com/corinfinite/llmify";
-        #   license = licenses.mit;  # Choose the appropriate license
-        #   maintainers = [ maintainers.yourusername ];
-        };
-      };
+      devShells = forAllSystems (
+        system:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
+        in
+        {
+          default = pkgs.${system}.mkShellNoCC {
+            packages = with pkgs.${system}; [
+              (mkPoetryEnv { projectDir = self; })
+              poetry
+            ];
+          };
+        }
+      );
     };
 }
